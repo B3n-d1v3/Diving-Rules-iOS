dd6b45fd03d88c89d3d2baf43e15d2f6a78b203a
Benoit SUZANNE <benoit.suzanne@orange.com>
2021-03-11 09:12:22 +0100

    Initial Commit

---
+++ Diving Rules/Diving_RulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diving_RulesApp.swift
+//  Diving Rules
+//
+//  Created by Benoit SUZANNE on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Diving_RulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
