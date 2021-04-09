eb4aae272cd8eb24b7a4d1afc31ef9231cdba94c
Benoit SUZANNE <ben.diving.judge@gmail.com>
2021-04-09 12:19:48 +0200

    Joined all stickers into one view

--- Diving Rules/Views/Stickers/Sticker.swift
+++ Diving Rules/Views/Stickers/Sticker.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct Sticker: View {
     var stickerImage: String
-//    var stikerColor: Int
+    var stickerColor: String
     var stickerText: String
+//    var color: String
     var body: some View {
         ZStack {
             Image (systemName: "shield.fill")
                 .resizable()
-                .frame(width: 85.0, height: 100.0)
+                .frame(width: 95.0, height: 110.0)
 //            if stickerColor == 0 {
-                .foregroundColor(.green)
+                .foregroundColor(Color(stickerColor))
 //                .foregroundColor(.orange)
 //                .foregroundColor(.red)
 //            }
@@ -39,6 +40,9 @@ struct Sticker: View {
 
 struct Sticker_Previews: PreviewProvider {
     static var previews: some View {
-        Sticker(stickerImage: "checkmark.circle", stickerText: "Good")
+        // Good: "checkmark.circle"
+        // Bad: "xmark.octagon.fill"
+        // Almost: "exclamationmark.triangle.fill"
+        Sticker(stickerImage: "checkmark.circle", stickerColor: "Good", stickerText: "Good")
     }
 }
